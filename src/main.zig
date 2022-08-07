f31de2527c4739333e0668854c360dac24a67de4
Noah Ryan <nsmryan@gmail.com>
2022-08-07 10:33:33 -0400

    Towards translation from Rust

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,14 @@ const testing = std.testing;
 
 /// This Pos type is expected to be generate enough to allow the user
 /// to cast into and out of their own type, such as from the euclid crate.
-pub Pos = struct { x: isize, y: isize };
+pub const Pos = struct {
+    x: isize,
+    y: isize,
 
+    pub fn new(x: isize, y: isize) Pos {
+        return Pos{ .x = x, .y = y };
+    }
+};
 
 /// Compute FOV information for a given position using the shadow mapping algorithm.
 ///
@@ -19,76 +25,75 @@ pub Pos = struct { x: isize, y: isize };
 ///
 /// I tried to write a nicer API which would modify the map as a separate user
 /// data, but I can't work out the lifetime annotations.
-pub fn ComputeFov(comptime Map: type, comptime Result: type) {
-    return struct {
-        const Self = @This;
-
-        map: Map,
-        result: Result,
-
-        pub const MarkVisible = fn (pos: Pos, map: Map) void;
-
-        pub const IsBlocking = fn (pos: Pos, result: Result) void;
-
-        pub fn init(map: Map, result: Result) Self {
-            return Self { .map = map, .result = result };
-        }
-
-        pub fn compute_fov(origin: Pos, is_blocking: IsBlocking, mark_visible: MarkVisible) void {
-            mark_visible(origin, self.map);
-
-            var index = 0;
-            while (index < 4) |index += 1| {
-                let quadrant = Quadrant.init(Cardinal.from_index(i), origin);
-
-                //const first_row = Row.init(1, Rational::new(-1, 1), Rational::new(1, 1));
-                const first_row = Row.init(1, -1, 1);
-
-                scan(first_row, quadrant, is_blocking, mark_visible);
-            }
-        }
-
-        fn scan(row: Row, quadrant: Quadrant, is_blocking: IsBlocking, mark_visible: MarkVisible) void {
-            let mut prev_tile = None;
-
-            let mut row = row;
-
-            for tile in row.tiles() {
-                let tile_is_wall = is_blocking(quadrant.transform(tile));
-                let tile_is_floor = !tile_is_wall;
-
-                let prev_is_wall = prev_tile.map_or(false, |prev| is_blocking(quadrant.transform(prev)));
-                let prev_is_floor = prev_tile.map_or(false, |prev| !is_blocking(quadrant.transform(prev)));
-
-                if tile_is_wall || is_symmetric(row, tile) {
-                    let pos = quadrant.transform(tile);
-
-                    mark_visible(pos);
-                }
-
-                if prev_is_wall && tile_is_floor {
-                    row.start_slope = slope(tile);
-                }
-
-                if prev_is_floor && tile_is_wall {
-                    let mut next_row = row.next();
-
-                    next_row.end_slope = slope(tile);
-
-                    scan(next_row, quadrant, is_blocking, mark_visible);
-                }
-
-                prev_tile = Some(tile);
-            }
-                
-            if prev_tile.map_or(false, |tile| !is_blocking(quadrant.transform(tile))) {
-                scan(row.next(), quadrant, is_blocking, mark_visible);
-            }
-        }
-
-    };
-}
-
+//pub fn ComputeFov(comptime Map: type, comptime Result: type) {
+//    return struct {
+//        const Self = @This;
+//
+//        map: Map,
+//        result: Result,
+//
+//        pub const MarkVisible = fn (pos: Pos, map: Map) void;
+//
+//        pub const IsBlocking = fn (pos: Pos, result: Result) void;
+//
+//        pub fn init(map: Map, result: Result) Self {
+//            return Self { .map = map, .result = result };
+//        }
+//
+//        pub fn compute_fov(origin: Pos, is_blocking: IsBlocking, mark_visible: MarkVisible) void {
+//            mark_visible(origin, self.map);
+//
+//            var index = 0;
+//            while (index < 4) |index += 1| {
+//                let quadrant = Quadrant.init(Cardinal.from_index(i), origin);
+//
+//                //const first_row = Row.init(1, Rational::new(-1, 1), Rational::new(1, 1));
+//                const first_row = Row.init(1, -1, 1);
+//
+//                scan(first_row, quadrant, is_blocking, mark_visible);
+//            }
+//        }
+//
+//        fn scan(row: Row, quadrant: Quadrant, is_blocking: IsBlocking, mark_visible: MarkVisible) void {
+//            var prev_tile = None;
+//
+//            var row = row;
+//
+//            for tile in row.tiles() {
+//                let tile_is_wall = is_blocking(quadrant.transform(tile));
+//                let tile_is_floor = !tile_is_wall;
+//
+//                let prev_is_wall = prev_tile.map_or(false, |prev| is_blocking(quadrant.transform(prev)));
+//                let prev_is_floor = prev_tile.map_or(false, |prev| !is_blocking(quadrant.transform(prev)));
+//
+//                if tile_is_wall || is_symmetric(row, tile) {
+//                    let pos = quadrant.transform(tile);
+//
+//                    mark_visible(pos);
+//                }
+//
+//                if prev_is_wall && tile_is_floor {
+//                    row.start_slope = slope(tile);
+//                }
+//
+//                if prev_is_floor && tile_is_wall {
+//                    let mut next_row = row.next();
+//
+//                    next_row.end_slope = slope(tile);
+//
+//                    scan(next_row, quadrant, is_blocking, mark_visible);
+//                }
+//
+//                prev_tile = Some(tile);
+//            }
+//
+//            if prev_tile.map_or(false, |tile| !is_blocking(quadrant.transform(tile))) {
+//                scan(row.next(), quadrant, is_blocking, mark_visible);
+//            }
+//        }
+//
+//    };
+//}
 
 const Cardinal = enum {
     North,
@@ -97,7 +102,8 @@ const Cardinal = enum {
     West,
 
     fn from_index(index: usize) Cardinal {
-        return { Cardinal.North, Cardinal.East, Cardinal.South, Cardinal.West }[index];
+        const cardinals = [4]Cardinal{ Cardinal.North, Cardinal.East, Cardinal.South, Cardinal.West };
+        return cardinals[index];
     }
 };
 
@@ -107,7 +113,7 @@ const Quadrant = struct {
     oy: isize,
 
     fn new(cardinal: Cardinal, origin: Pos) Quadrant {
-        return Quadrant { .cardinal, .ox: origin.x, oy: .origin.y };
+        return Quadrant{ .cardinal = cardinal, .ox = origin.x, .oy = origin.y };
     }
 
     fn transform(self: *Quadrant, tile: Pos) Pos {
@@ -115,188 +121,213 @@ const Quadrant = struct {
         const col = tile.y;
 
         switch (self.cardinal) {
-            Cardinal::North => {
-                return (self.ox + col, self.oy - row);
+            Cardinal.North => {
+                return Pos.new(self.ox + col, self.oy - row);
             },
 
-            Cardinal::South => {
-                return (self.ox + col, self.oy + row);
+            Cardinal.South => {
+                return Pos.new(self.ox + col, self.oy + row);
             },
 
-            Cardinal::East => {
-                return (self.ox + row, self.oy + col);
+            Cardinal.East => {
+                return Pos.new(self.ox + row, self.oy + col);
             },
 
-            Cardinal::West => {
-                return (self.ox - row, self.oy + col);
+            Cardinal.West => {
+                return Pos.new(self.ox - row, self.oy + col);
             },
         }
     }
 };
 
-const Row = struct {
-    depth: isize,
-    start_slope: Rational,
-    end_slope: Rational,
-
-    fn new(depth: isize, start_slope: Rational, end_slope: Rational) Row {
-        return Row { depth, start_slope, end_slope };
-    }
-
-    fn tiles(self: *Row) -> impl Iterator<Item=Pos> {
-        const depth_times_start = Rational::new(self.depth, 1) * self.start_slope;
-        const depth_times_end = Rational::new(self.depth, 1) * self.end_slope;
-
-        const min_col = round_ties_up(depth_times_start);
-
-        const max_col = round_ties_down(depth_times_end);
-
-        const depth = self.depth;
-
-        return (min_col..=max_col).map(move |col| (depth, col));
-    }
-
-    fn next(self: *Row) -> Row {
-        return Row.init(self.depth + 1, self.start_slope, self.end_slope);
-    }
-}
+//const Row = struct {
+//    depth: isize,
+//    start_slope: Rational,
+//    end_slope: Rational,
+//
+//    fn new(depth: isize, start_slope: Rational, end_slope: Rational) Row {
+//        return Row { depth, start_slope, end_slope };
+//    }
+//
+//    // NOTE the Rust version uses an interator. For Zig, I will likely define a separate
+//    // type for this, or look at idiomatic iterators.
+//    fn tiles(self: *Row) impl Iterator<Item=Pos> {
+//        const depth_times_start = Rational::new(self.depth, 1) * self.start_slope;
+//        const depth_times_end = Rational::new(self.depth, 1) * self.end_slope;
+//
+//        const min_col = round_ties_up(depth_times_start);
+//
+//        const max_col = round_ties_down(depth_times_end);
+//
+//        const depth = self.depth;
+//
+//        return (min_col..=max_col).map(move |col| (depth, col));
+//    }
+//
+//    fn next(self: *Row) Row {
+//        return Row.init(self.depth + 1, self.start_slope, self.end_slope);
+//    }
+//}
 
 fn slope(tile: Pos) Rational {
-    const (row_depth, col) = tile;
+    const row_depth = tile.x;
+    const col = tile.y;
     return Rational.init(2 * col - 1, 2 * row_depth);
 }
 
 fn is_symmetric(row: Row, tile: Pos) bool {
     const col = tile.y;
 
-    const depth_times_start = Rational::new(row.depth, 1) * row.start_slope;
-    const depth_times_end = Rational::new(row.depth, 1) * row.end_slope;
+    const depth_times_start = Rational.new(row.depth, 1) * row.start_slope;
+    const depth_times_end = Rational.new(row.depth, 1) * row.end_slope;
 
-    const col_rat = Rational::new(col, 1);
+    const col_rat = Rational.new(col, 1);
 
-    const symmetric = col_rat >= depth_times_start && col_rat <= depth_times_end;
+    const symmetric = col_rat >= depth_times_start and col_rat <= depth_times_end;
 
     return symmetric;
 }
 
 fn round_ties_up(n: Rational) isize {
-    return (n + Rational.init(1, 2)).floor().to_integer();
+    return (n + Rational.new(1, 2)).floor();
 }
 
 fn round_ties_down(n: Rational) isize {
-    return (n - Rational.init(1, 2)).ceil().to_integer();
+    return (n - Rational.new(1, 2)).ceil();
 }
 
-#[cfg(test)]
-fn inside_map<T>(pos: Pos, map: &Vec<Vec<T>>) bool {
-    let is_inside = (pos.1 as usize) < map.len() && (pos.0 as usize) < map[0].len();
-    return is_inside;
-}
+const Rational = struct {
+    num: isize,
+    denom: isize,
 
-#[cfg(test)]
-fn matching_visible(expected: Vec<Vec<usize>>, visible: Vec<(isize, isize)>) {
-    for y in 0..expected.len() {
-        for x in 0..expected[0].len() {
-            if visible.contains(&(x as isize, y as isize)) {
-                print!("1");
-            } else {
-                print!("0");
-            }
-            assert_eq!(expected[y][x] == 1, visible.contains(&(x as isize, y as isize)));
-        }
-        println!();
+    pub fn new(num: isize, denom: isize) Rational {
+        return Rational{ .num = num, .denom = denom };
     }
-}
 
-#[test]
-fn test_expansive_walls() {
-    let origin = (1, 2);
+    pub fn mult(self: Rational, other: Rational) Rational {}
 
-    let tiles = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
-                     vec!(1, 0, 0, 0, 0, 0, 1),
-                     vec!(1, 0, 0, 0, 0, 0, 1),
-                     vec!(1, 1, 1, 1, 1, 1, 1));
+    pub fn add(self: Rational, other: Rational) Rational {}
 
-    let mut is_blocking = |pos: Pos| {
-        return  !inside_map(pos, &tiles) || tiles[pos.1 as usize][pos.0 as usize] == 1;
-    };
+    pub fn ceil(self: Rational) isize {
+        const div = self.num / self.denom;
+        return div + @boolToInt(self.num % self.denom > 0);
+    }
 
-    let mut visible = Vec::new();
-    let mut mark_visible = |pos: Pos| {
-        if inside_map(pos, &tiles) && !visible.contains(&pos) {
-            visible.push(pos);
-        }
-    };
+    pub fn floor(self: Rational) isize {
+        return self.num / self.denom;
+    }
+};
 
-    compute_fov(origin, &mut is_blocking, &mut mark_visible);
-
-    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 1, 1, 1, 1, 1));
-    matching_visible(expected, visible);
-}
-
-
-#[test]
-fn test_expanding_shadows() {
-    let origin = (0, 0);
-
-    let tiles = vec!(vec!(0, 0, 0, 0, 0, 0, 0),
-                     vec!(0, 1, 0, 0, 0, 0, 0),
-                     vec!(0, 0, 0, 0, 0, 0, 0),
-                     vec!(0, 0, 0, 0, 0, 0, 0),
-                     vec!(0, 0, 0, 0, 0, 0, 0));
-
-    let mut is_blocking = |pos: Pos| {
-        return !inside_map(pos, &tiles) || tiles[pos.1 as usize][pos.0 as usize] == 1;
-    };
-
-    let mut visible = Vec::new();
-    let mut mark_visible = |pos: Pos| {
-        if inside_map(pos, &tiles) && !visible.contains(&pos) {
-            visible.push(pos);
-        }
-    };
-
-    compute_fov(origin, &mut is_blocking, &mut mark_visible);
-
-    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 0, 0, 1, 1, 1),
-                        vec!(1, 1, 0, 0, 0, 0, 1),
-                        vec!(1, 1, 1, 0, 0, 0, 0));
-    matching_visible(expected, visible);
-}
-
-#[test]
-fn test_no_blind_corners() {
-    let origin = (3, 0);
-
-    let tiles = vec!(vec!(0, 0, 0, 0, 0, 0, 0),
-                     vec!(1, 1, 1, 1, 0, 0, 0),
-                     vec!(0, 0, 0, 1, 0, 0, 0),
-                     vec!(0, 0, 0, 1, 0, 0, 0));
-
-    let mut is_blocking = |pos: Pos| {
-        let outside = (pos.1 as usize) >= tiles.len() || (pos.0 as usize) >= tiles[0].len();
-        return  outside || tiles[pos.1 as usize][pos.0 as usize] == 1;
-    };
-
-    let mut visible = Vec::new();
-    let mut mark_visible = |pos: Pos| {
-        let outside = (pos.1 as usize) >= tiles.len() || (pos.0 as usize) >= tiles[0].len();
-
-        if !outside && !visible.contains(&pos) {
-            visible.push(pos);
-        }
-    };
-
-    compute_fov(origin, &mut is_blocking, &mut mark_visible);
-
-    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(1, 1, 1, 1, 1, 1, 1),
-                        vec!(0, 0, 0, 0, 1, 1, 1),
-                        vec!(0, 0, 0, 0, 0, 1, 1));
-    matching_visible(expected, visible);
-}
+//#[cfg(test)]
+//fn inside_map<T>(pos: Pos, map: &Vec<Vec<T>>) bool {
+//    let is_inside = (pos.1 as usize) < map.len() && (pos.0 as usize) < map[0].len();
+//    return is_inside;
+//}
+//
+//#[cfg(test)]
+//fn matching_visible(expected: Vec<Vec<usize>>, visible: Vec<(isize, isize)>) {
+//    for y in 0..expected.len() {
+//        for x in 0..expected[0].len() {
+//            if visible.contains(&(x as isize, y as isize)) {
+//                print!("1");
+//            } else {
+//                print!("0");
+//            }
+//            assert_eq!(expected[y][x] == 1, visible.contains(&(x as isize, y as isize)));
+//        }
+//        println!();
+//    }
+//}
+//
+//#[test]
+//fn test_expansive_walls() {
+//    let origin = (1, 2);
+//
+//    let tiles = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
+//                     vec!(1, 0, 0, 0, 0, 0, 1),
+//                     vec!(1, 0, 0, 0, 0, 0, 1),
+//                     vec!(1, 1, 1, 1, 1, 1, 1));
+//
+//    let mut is_blocking = |pos: Pos| {
+//        return  !inside_map(pos, &tiles) || tiles[pos.1 as usize][pos.0 as usize] == 1;
+//    };
+//
+//    let mut visible = Vec::new();
+//    let mut mark_visible = |pos: Pos| {
+//        if inside_map(pos, &tiles) && !visible.contains(&pos) {
+//            visible.push(pos);
+//        }
+//    };
+//
+//    compute_fov(origin, &mut is_blocking, &mut mark_visible);
+//
+//    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 1, 1, 1, 1, 1));
+//    matching_visible(expected, visible);
+//}
+//
+//
+//#[test]
+//fn test_expanding_shadows() {
+//    let origin = (0, 0);
+//
+//    let tiles = vec!(vec!(0, 0, 0, 0, 0, 0, 0),
+//                     vec!(0, 1, 0, 0, 0, 0, 0),
+//                     vec!(0, 0, 0, 0, 0, 0, 0),
+//                     vec!(0, 0, 0, 0, 0, 0, 0),
+//                     vec!(0, 0, 0, 0, 0, 0, 0));
+//
+//    let mut is_blocking = |pos: Pos| {
+//        return !inside_map(pos, &tiles) || tiles[pos.1 as usize][pos.0 as usize] == 1;
+//    };
+//
+//    let mut visible = Vec::new();
+//    let mut mark_visible = |pos: Pos| {
+//        if inside_map(pos, &tiles) && !visible.contains(&pos) {
+//            visible.push(pos);
+//        }
+//    };
+//
+//    compute_fov(origin, &mut is_blocking, &mut mark_visible);
+//
+//    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 0, 0, 1, 1, 1),
+//                        vec!(1, 1, 0, 0, 0, 0, 1),
+//                        vec!(1, 1, 1, 0, 0, 0, 0));
+//    matching_visible(expected, visible);
+//}
+//
+//#[test]
+//fn test_no_blind_corners() {
+//    let origin = (3, 0);
+//
+//    let tiles = vec!(vec!(0, 0, 0, 0, 0, 0, 0),
+//                     vec!(1, 1, 1, 1, 0, 0, 0),
+//                     vec!(0, 0, 0, 1, 0, 0, 0),
+//                     vec!(0, 0, 0, 1, 0, 0, 0));
+//
+//    let mut is_blocking = |pos: Pos| {
+//        let outside = (pos.1 as usize) >= tiles.len() || (pos.0 as usize) >= tiles[0].len();
+//        return  outside || tiles[pos.1 as usize][pos.0 as usize] == 1;
+//    };
+//
+//    let mut visible = Vec::new();
+//    let mut mark_visible = |pos: Pos| {
+//        let outside = (pos.1 as usize) >= tiles.len() || (pos.0 as usize) >= tiles[0].len();
+//
+//        if !outside && !visible.contains(&pos) {
+//            visible.push(pos);
+//        }
+//    };
+//
+//    compute_fov(origin, &mut is_blocking, &mut mark_visible);
+//
+//    let expected = vec!(vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(1, 1, 1, 1, 1, 1, 1),
+//                        vec!(0, 0, 0, 0, 1, 1, 1),
+//                        vec!(0, 0, 0, 0, 0, 1, 1));
+//    matching_visible(expected, visible);
+//}
