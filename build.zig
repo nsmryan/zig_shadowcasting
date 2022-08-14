95862ec2405a6b69b100a5060e6b1deb956118fb
Noah Ryan <nsmryan@gmail.com>
2022-08-14 11:01:43 -0400

    Minimal example of inference giving a weird result

--- build.zig
+++ build.zig
@@ -4,14 +4,15 @@ pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
 
-    const lib = b.addStaticLibrary("zig_shadowcasting", "src/main.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    const exe = b.addExecutable("example", "src/main.zig");
+    exe.setBuildMode(mode);
+    exe.setTarget(target);
+    exe.install();
 
-    const main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    const run_cmd = exe.run();
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const run_step = b.step("run", "Run example");
+    run_step.dependOn(&run_cmd.step);
 }
